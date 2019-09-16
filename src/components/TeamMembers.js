d84949ef4a29c76897b51e0b275c1837a492bd3d
BuildTools <arthurpisakhov1@gmail.com>
2019-09-16 16:11:45 -0400

    basics

---
+++ src/components/TeamMembers.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function TeamMembers() {
+  const [team, setTeam] = useState([]);
+
+
+  return (
+    <div>
+      Team Members are here!
+    </div>
+  )
+}
+
+export default TeamMembers;
